05fcad162b7dbf616feb8954d5e9d4c1df8b1fca
Rody Davis <rody.davis.jr@gmail.com>
2019-05-31 11:26:15 -0400

    removing after layout

--- lib/templates/3-column/three_column_navigation.dart
+++ lib/templates/3-column/three_column_navigation.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:scroll_to_index/scroll_to_index.dart';
-import 'package:after_layout/after_layout.dart';
 
 import 'common/index.dart';
 
